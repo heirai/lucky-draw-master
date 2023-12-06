2fbbb47685262e79d8d77372fc8ccb4016a52ec5
tom_glb <w32791102_glb@163.com>
2023-12-06 13:44:21 +0900

    config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-  publicPath: './' // vue-cli3项目如果有该项配置，则将值修改为‘./’, 没有则添加
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/lucky-draw-master/' : '/'
 };
